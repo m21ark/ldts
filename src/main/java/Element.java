6cc1e722ba31cdcd912a497a9e92db5eb9e44ff4
Marco <marco14lyoko@gmail.com>
2022-01-05 20:09:17 +0000

    Implemented ClassElement

--- src/main/java/Element.java
+++ src/main/java/Element.java
@@ -11,16 +11,19 @@ public class Element {
     protected Position position = new Position(0, 0);
 
     public Element(int x, int y, Character character, String color) {
-
+        this.position.setX(x);
+        this.position.setY(y);
+        this.character = character;
+        this.color = color;
     }
 
     public Element(Position pos, Character character, String color) {
-
+        this.position.set(pos );
+        this.character = character;
+        this.color = color;
     }
 
-    public void gravityMoveDown() {
-
-    }
+    public void gravityMoveDown() {position.setY(position.getY()+1);}
 
     public Position getPosition() {return position;}
     public int getPositionX() {return position.getX();}
@@ -28,19 +31,29 @@ public class Element {
     public Character getChar(){return character;}
     public  boolean isFixedPos(){return fixedPos;}
 
+    public Element setFixedPos(boolean newFixedPos) {this.fixedPos = newFixedPos;
+        return this;
+    }
     public void setChar(Character newChar) {this.character = newChar;}
 
     public void setPos(Position newPos){
         position  = newPos;
     }
 
-    public Element setFixedPos(boolean newFixedPos) {
-        return null;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        if (getClass() != o.getClass()) return false;
+        Element p = (Element) o;
+        return character == p.getChar() &&  p.getPosition().equals(position) && fixedPos == p.isFixedPos();
     }
 
-
     public void draw(TextGraphics graphics) {
+        graphics.setForegroundColor(TextColor.Factory.fromString(this.color));
+        graphics.enableModifiers(SGR.BOLD);
 
+        graphics.putString(new TerminalPosition(this.position.getX(), this.position.getY()), this.character.toString());
     }
 
 
