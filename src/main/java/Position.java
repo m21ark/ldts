f27f3396c6a4e2557a943b3140df0b7a33a321d3
Marco <marco14lyoko@gmail.com>
2022-01-05 19:59:37 +0000

    Implemented ClassPositon

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -3,27 +3,37 @@ public class Position {
     private int x, y;
 
     Position(int x, int y) {
-
+        this.x = x;
+        this.y = y;
     }
 
     public int getY() {
-        return 0;
+        return y;
     }
 
     public void setY(int y) {
-
+        this.y = y;
     }
 
     public int getX() {
-        return 0;
+        return x;
     }
 
     public void setX(int x) {
-
+        this.x = x;
     }
 
-    public void set(Position pos) {
-
+    public void set(Position pos){
+       setX(pos.getX());
+       setY(pos.getY());
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        if (getClass() != o.getClass()) return false;
+        Position p = (Position) o;
+        return x == p.getX() && y == p.getY();
+    }
 }
