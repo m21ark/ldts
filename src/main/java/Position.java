acc4c658c14b21b7a77e0ba9299676965b84b14b
Marco <marco14lyoko@gmail.com>
2022-01-05 19:54:32 +0000

    Added ClassPosition Strucure

---
+++ src/main/java/Position.java
@@ -0,0 +1,29 @@
+public class Position {
+
+    private int x, y;
+
+    Position(int x, int y) {
+
+    }
+
+    public int getY() {
+        return 0;
+    }
+
+    public void setY(int y) {
+
+    }
+
+    public int getX() {
+        return 0;
+    }
+
+    public void setX(int x) {
+
+    }
+
+    public void set(Position pos) {
+
+    }
+
+}
