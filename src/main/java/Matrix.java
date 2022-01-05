e2bf84a7c5cc4fafe31f570399cd77313250a00e
Marco <marco14lyoko@gmail.com>
2022-01-05 20:33:26 +0000

    Implemented ClassMatrix

--- src/main/java/Matrix.java
+++ src/main/java/Matrix.java
@@ -4,31 +4,49 @@ import java.util.List;
 public class Matrix {
 
     private final List<List<Element>> matrix = new ArrayList<>();
-    private  int width;
-    private  int height;
+    private final int width;
+    private final int height;
 
     Matrix(int width, int height, Character defaultChar) {
+        this.width = width;
+        this.height = height;
 
+        for (int i = 0; i < height; i++) {
+            List<Element> aux = new ArrayList<>();
+            for (int j = 0; j < width; j++)
+                aux.add(new Element(i, j, defaultChar, "#000000"));
+            matrix.add(aux);
+        }
     }
 
     public int getWidth() {
-        return 0;
+        return width;
     }
 
     public int getHeight() {
-        return 0;
+        return height;
     }
 
     public boolean setPos(Element c) {
-        return false;
+        int x = c.getPosition().getX();
+        int y = c.getPosition().getY();
+
+        if (x < 0 || y < 0 || x >= width || y >= height) return false;
+
+        matrix.get(y).set(x, c);
+        return true;
     }
 
     public Element getPos(int x, int y) {
-        return null;
+        if (x < 0 || y < 0 || x >= width || y >= height)
+            return new Element(-1, -1, 'N', "#FFFFFF"); // null Elem
+        return matrix.get(y).get(x);
     }
 
     public Element getPos(Position pos) {
-        return null;
+        if (pos.getX() < 0 || pos.getY() < 0 || pos.getX() >= width || pos.getY() >= height)
+            return new Element(-1, -1, 'N', "#FFFFFF"); // null Elem
+        return matrix.get(pos.getY()).get(pos.getX());
     }
 
     public List<List<Element>> getMatrix() {
@@ -37,7 +55,11 @@ public class Matrix {
 
 
     public void print() {
-
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++)
+                System.out.print(matrix.get(i).get(j).getChar());
+            System.out.println();
+        }
     }
 
 
