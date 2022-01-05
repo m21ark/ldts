f3f34feb4589d98d936f06ee8fcd938066541360
Marco <marco14lyoko@gmail.com>
2022-01-05 20:45:28 +0000

    Added ClassCoin Structure

---
+++ src/main/java/Coin.java
@@ -0,0 +1,10 @@
+public class Coin extends Element {
+
+    public Coin(int x, int y, Character character, String color) {
+        super(x, y, character, color);
+    }
+    public Coin(Position pos, Character character, String color) {
+        super(pos, character, color);
+    }
+
+}
