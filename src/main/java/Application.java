d825b0c3f9cdc06c7b439d64f4918419be33d5dd
Marco <marco14lyoko@gmail.com>
2022-01-06 21:09:12 +0000

    Added a game font

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,15 +1,17 @@
+import java.awt.*;
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class Application {
 
     public static void main(String[] args) {
 
         try {
-            Game game = new Game(70, 30);
+            Game game = new Game(50, 35);
             game.run();
             System.exit(0);
 
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException | FontFormatException e) {
             e.printStackTrace();
         }
 
